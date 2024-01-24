83af53acb1db1acf7a468c4eb3fb720f662add04
isa-gooijer <60418025+isa-gooijer@users.noreply.github.com>
2024-01-24 00:41:49 -0500

    update python

--- application.py
+++ application.py
@@ -2,6 +2,8 @@
 # part 1: https://medium.com/@anupbhande/create-a-static-website-on-github-using-python-and-flask-framework-part-1-e26c0d39bec2
 # part 2: https://medium.com/@anupbhande/create-a-static-website-on-github-using-python-and-flask-part-2-335d6df42def
 
+# Import relevant libraries
+# *** Need to the delete the irrelevant ones ***
 
 import os
 
@@ -36,32 +38,32 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-
+# *** MIGHT HAVE TO CHANGE THIS?! ***
 # Make sure API key is set
 if not os.environ.get("API_KEY"):
     raise RuntimeError("API_KEY not set")
 
 
-#HOMEPAGE
+# Homepage
 @app.route("/")
 def index():
     # get the homepage of the website.
     return render_template("index.html")
 
 
-#About
+# About
 @app.route("/about")
 def about():
     # get about page
     return render_template("index.html")
 
-#01_intro
+# 01_intro
 @app.route("/intro")
 def intro():
     return render_template("/assignment/01_intro.html")
 
 
-#ERROR HANDLERs
+# ERROR HANDLERs
 def errorhandler(e):
     """Handle error"""
     if not isinstance(e, HTTPException):
