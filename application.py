6e0a1a471547c03ed696b5105cf9a0137193f892
isa-gooijer <60418025+isa-gooijer@users.noreply.github.com>
2024-01-23 23:51:50 -0500

    implement python based webpage

---
+++ application.py
@@ -0,0 +1,69 @@
+import os
+
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import apology, login_required
+
+# to get the date and time for the transactions.
+from datetime import datetime
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+# Make sure API key is set
+if not os.environ.get("API_KEY"):
+    raise RuntimeError("API_KEY not set")
+
+
+#HOMEPAGE
+@app.route("/")
+def index():
+    # get the homepage of the website.
+    return render_template("index.html")
+
+
+#About
+@app.route("/about")
+def about():
+    # get about page
+    return render_template("index.html")
+
+#01_intro
+@app.route("/intro")
+def intro():
+    return render_template("/assignment/01_intro.html")
+
+
+#ERROR HANDLERs
+def errorhandler(e):
+    """Handle error"""
+    if not isinstance(e, HTTPException):
+        e = InternalServerError()
+    return apology(e.name, e.code)
+
+
+# Listen for errors
+for code in default_exceptions:
+    app.errorhandler(code)(errorhandler)
